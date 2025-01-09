77910106a85c0937953458a4d0b649c4556667a4
PaynePew <nlpam06@gmail.com>
2025-01-09 19:43:41 +0800

    chore(style): add spacing template

--- components/BaseHeader.vue
+++ components/BaseHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div>
-      <nav className="w-full flex justify-center gap-[32px]">
+      <nav className="w-full flex justify-center gap-[32px] text-neutral-b-500">
         <NuxtLink to="/">
           <span>Home</span>
         </NuxtLink>
