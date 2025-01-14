1653f3c9070acadac34a4b449cc3d05772892d41
PaynePew <nlpam06@gmail.com>
2025-01-14 21:24:14 +0800

    feat(header): update header style

--- components/BaseHeader.vue
+++ components/BaseHeader.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="w-full">
     <div class="container mx-auto flex items-center justify-center">
+      <!--Advertisement-->
       <div
         class="w-full absolute top-0 h-[40px] flex justify-center items-center bg-neutral-b-900 text-neutral-w-900 text-[14px] font-normal"
       >
         Get 25% OFF on your first order. Order Now
       </div>
+      <!--Header-->
       <header
         class="flex flex-1 justify-between max-w-[1116px] h-[84px] mt-[40px]"
       >
         <div class="flex flex-1 justify-center items-center">
+          <!--Logo Header-->
           <div
             class="flex justify-center items-center min-w-[163px] min-h-[40px]"
           >
             <img src="/public/Logo-Header.svg" alt="logo" />
           </div>
+          <!--Navigation-->
           <nav
             class="w-full flex justify-center items-center gap-07 text-neutral-b-500 drop-shadow-nav font-medium"
           >
@@ -31,12 +35,19 @@
               <span>Contact</span>
             </NuxtLink>
           </nav>
-          <div class="flex">
+          <!--User & Cart & Searching-->
+          <div class="flex flex-1 gap-07">
             <input
-              class="border-2 border-neutral-b-100 rounded-6 focus:border-neutral-b-300 focus:outline-none"
+              class="min-w-[264px] h-[45px] border-2 border-neutral-b-100 rounded-[4px] focus:border-neutral-b-300 focus:outline-none"
             />
-            <img src="/public/Cart.svg" alt="cart" />
-            <img src="/public/User.svg" alt="user" />
+            <div class="w-full flex flex-1 gap-07 justify-center items-center">
+              <div class="w-[18px] h-[18px]">
+                <img src="/public/Cart.svg" alt="cart" />
+              </div>
+              <div class="w-[18px] h-[18px]">
+                <img src="/public/User.svg" alt="user" />
+              </div>
+            </div>
           </div>
         </div>
       </header>
