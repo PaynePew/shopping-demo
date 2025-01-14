809a12399df2b9319ad9c69437768ddbf61caa8d
PaynePew <nlpam06@gmail.com>
2025-01-14 20:31:42 +0800

    feat(login): add login page template

---
+++ pages/login.vue
@@ -0,0 +1,69 @@
+<template>
+  <!--Breadcrumb-->
+  <section class="w-full bg-neutral-w-100">
+    <div
+      class="container pl-04 my-[16px] flex flex-1 flex-col text-[14px] font-medium"
+    >
+      <div class="prose mb-03"><h3>Login</h3></div>
+      <div class="flex flex-1 items-center">
+        <div class="text-neutral-b-500">Ecommerce</div>
+        <div>
+          <img src="/Chevron-Right.svg" alt="chevron right" />
+        </div>
+        <div class="text-neutral-b-900">Login</div>
+      </div>
+    </div>
+  </section>
+
+  <!--Login Form-->
+  <section class="w-full">
+    <div class="container mx-auto pt-[128px] pb-[148px] flex justify-center">
+      <div class="w-[320px] flex flex-col justify-center items-center">
+        <div
+          class="w-full px-06 py-04 flex flex-1 justify-center items-center gap-03 border-solid border-[1px] border-neutral-b-200 rounded-[4px] mb-07"
+        >
+          <div><img src="/Google.svg" alt="google icon" /></div>
+          <div class="text-neutral-b-500 text-[14px] font-medium">
+            Continue with Google
+          </div>
+        </div>
+        <div class="w-full flex flex-1 justify-center items-center mb-[34px]">
+          <div class="h-[1px] w-full bg-neutral-b-100"></div>
+          <div class="mx-05 text-neutral-b-500 text-[12px]">OR</div>
+          <div class="h-[1px] w-full bg-neutral-b-100"></div>
+        </div>
+        <div class="w-full flex flex-col gap-[15px] mb-[16px]">
+          <div class="flex flex-col">
+            <label class="text-neutral-b-600">Email</label>
+            <input
+              class="h-fit border-[1px] border-neutral-b-100 px-[15px] py-[10px] rounded-[6px] focus:outline-none"
+            />
+          </div>
+          <div class="flex flex-col">
+            <label class="text-neutral-b-600">Password</label>
+            <input
+              class="h-fit border-[1px] border-neutral-b-100 px-[15px] py-[10px] rounded-[6px] focus:outline-none"
+            />
+          </div>
+        </div>
+        <div class="flex self-end mb-[24px]">
+          <div class="font-medium text-[12px] text-neutral-b-600">
+            Forgot Password?
+          </div>
+        </div>
+        <div class="w-full mb-06">
+          <button
+            class="w-full h-[44px] px-06 py-04 bg-neutral-b-900 text-neutral-w-900 rounded-[4px] text-[14px] flex flex-1 justify-center items-center"
+          >
+            Login
+          </button>
+        </div>
+        <div class="flex">
+          <div class="font-medium text-[14px] text-neutral-b-500">
+            Don't have an account? Sign up
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
