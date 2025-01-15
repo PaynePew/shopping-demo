613b7757360ce7ae1a49e51a4baa84aab0ffce6d
PaynePew <nlpam06@gmail.com>
2025-01-15 15:49:24 +0800

    chore(nuxt config): add pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
   app: {
     head: {
       link: [
@@ -14,4 +14,3 @@ export default defineNuxtConfig({
     },
   },
 });
-
